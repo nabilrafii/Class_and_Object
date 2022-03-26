f4e25ae7d836e8f2812863ec96c58acd1dbc0187
nabilrafii <nabilrafii333@students.amikom.ac.id>
2022-03-26 17:16:49 +0700

    Tugas Taxi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Dan_Object_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Class_Dan_Object_Taxi.Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "Havel";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 93;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
